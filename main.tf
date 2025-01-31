9068ba1da970846daed4a9cd4dd513347d250fca
snehallpawar <100402255+snehallpawar@users.noreply.github.com>
2025-01-31 19:08:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,14 +17,16 @@ resource "aws_route" "private_route" {
   nat_gateway_id         = "nat-0a34a8efd5e420945"  # Replace with your actual NAT Gateway ID
 }
 
-# Lambda function in the private subnet
 resource "aws_lambda_function" "my_lambda" {
-  function_name = "MyLambdaFunction"
-  role          = data.aws_iam_role.lambda.arn
-  handler       = "index.handler"
-  runtime       = "python3.8"
-  filename      = "lambda_function.zip"  # Ensure your Lambda code is zipped and available
+  filename         = "path/to/your/lambda_function.zip"  # Update this path
+  function_name    = "MyLambdaFunction"
+  handler          = "index.handler"
+  runtime          = "python3.8"
+  role             = "arn:aws:iam::168009530589:role/DevOps-Candidate-Lambda-Role"
+  memory_size      = 128
+  timeout          = 3
 }
+
 
 output "private_subnet_id" {
   value = aws_subnet.private_subnet.id
